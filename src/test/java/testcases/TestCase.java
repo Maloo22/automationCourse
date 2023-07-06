d2e15eacb42b0a991014bb5c22d7dffe0c2579d1
AndrewEfimovich <113019976+Maloo22@users.noreply.github.com>
2023-07-06 23:54:40 +0300

    Initial commit

---
+++ src/test/java/testcases/TestCase.java
@@ -0,0 +1,22 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TestCase {
+
+    @Test
+    public void startBrowser() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Andrew\\IdeaProjects\\automationCourse\\src\\test\\resources\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+
+        driver.get("https://ttrsonline.com/Account/LogOn");
+        driver.manage().window().maximize();
+        driver.close();
+       
+
+    }
+}
